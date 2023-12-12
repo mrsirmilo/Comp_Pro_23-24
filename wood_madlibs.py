92e87141b98615e60f661119f9c058fe814a7b7a
Derek <dmwhmad@gmail.com>
2023-12-12 10:00:10 -0700

    Final Madlib Project

---
+++ wood_madlibs.py
@@ -0,0 +1,28 @@
+# Derek Wood
+# 2B
+# December 12, 2023
+
+# This program will create a story with a twist based on the user's inputs
+
+animal1 = input("Please input an animal ")
+adjective1 = input("Please input an adjective ")
+noun1 = input("Please input a plural noun ")
+verb1 = input("Please insert a verb ending in (-ing) ")
+color1 = input("Please input a color ")
+color2 = input("Please input another color ")
+noun2 = input("Please input another noun ")
+
+
+print(f"Once upon a time, in a land far, far away, there lived a curious little {animal1}.")
+
+print(f"This creature was known for its {adjective1} nature and its love for collecting shiny {noun1}.")
+
+print(f"Every day, it could be found {verb1} around the forest, searching for the most vibrant {color1} objects to add to its collection.")
+
+print(f"One day, while exploring near a sparkling stream, it stumbled upon a hidden cave filled with treasures of all shapes and sizes.")
+
+print(f"The cave was illuminated by the glow of precious stones in every {color2} hue imaginable. The little creature was overjoyed and began gathering these treasures, feeling like the luckiest {noun2} in the world.")
+
+print(f"From that day on, it shared its collection with all the animals in the forest, spreading happiness and wonder with its colorful discoveries. ")
+
+print(f"And so, the little creature and its magnificent collection became legends in the enchanted land, teaching everyone the true value of curiosity and sharing.")
